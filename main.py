32d711b2ec602a31363f5519511c035fd44be3b0
Fedi GALFAT <55464780+fozz101@users.noreply.github.com>
2021-08-10 11:35:42 +0100

    Added fetch G Drive feature for a Project folder

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from youtubesearchpython import VideosSearch
 import asyncpraw
 import random
 from keep_alive import keep_alive
-
+from DriveAPI import fetchData
 import requests
 import json
 
@@ -32,14 +32,24 @@ password=os.environ['password'],
 user_agent="bot")
 
 
-authorised_channel_id_memes=[868903933031104572,870324693494804571,835817984593100840,867890807759831060,870338494923440148]
+authorised_channel_id_memes=[868903933031104572,870324693494804571,835817984593100840,867890807759831060,870338494923440148,870404449699528714,873297121548337152]
 
-authorised_channel_id_music= [867880055182589975,868903933031104572,835817984593100840,870338494923440148]
+authorised_channel_id_music= [867880055182589975,868903933031104572,835817984593100840,870338494923440148,870404449699528714,873297121548337152]
 welcome_channel = 868902567705456661
 
 players = {}
 
+async def placement_error(ctx):
+  await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+  messages = await ctx.history(limit=2).flatten()
+  await asyncio.sleep(3)
+  await ctx.message.channel.delete_messages(messages)
 
+async def placement_error_meme(ctx):
+  await ctx.send("Sè7bi barra ekteb el command hedhi fi memes | 5oddim-cmd🤬")
+  messages = await ctx.history(limit=2).flatten()
+  await asyncio.sleep(3)
+  await ctx.message.channel.delete_messages(messages)
 
 @client.event
 async def on_ready():
@@ -103,8 +113,8 @@ async def on_member_join(member):
   #Select welcome channel !
   channel = client.get_channel(welcome_channel)
   
-  await channel.send(f"Yooo {tag}, mara7bee biik fi discord el CS !  😎🎉",file=discord.File("final.png"))
-  await member.send(f"Ahlaa ahla {tag}, mara7bee bik fi darek 😍\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.facebook.com/cs.esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.instagram.com/ieee.cs.esprit/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.linkedin.com/company/cs-esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttp://computer-esprit.ieee.tn/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nTansanèch fi follow/like ken mech 3amel 👀🤣\nFINALLY, 3andek fekra mte3 event wela project? T7eb t7assenni ?\nMy Master {tagFozz} yestana fik 🤩🤩")
+  await channel.send(f"Yooo {tag}, mara7bee biik fi discord el CS !  😎🎉. Ekteb !help bech ta3ref chnewa enajem nsarbik 7aliyan.",file=discord.File("final.png"))
+  await member.send(f"Ahlaa ahla {tag}, mara7bee bik fi darek 😍\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.facebook.com/cs.esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.instagram.com/cs.esprit/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.linkedin.com/company/cs-esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttp://computer-esprit.ieee.tn/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nTansanèch fi follow/like ken mech 3amel 👀🤣\nFINALLY, 3andek fekra mte3 event wela project? T7eb t7assenni ?\nMy Master Fozz yestana fik 🤩🤩")
  
 
 @client.event
@@ -122,10 +132,10 @@ async def on_message(message):
     await message.channel.send(f"mara7beee {tag}  ")
     
     #await channel.send("ahlaaa")
-    await usr.send(f"Ahlaa ahla {tag}, mara7bee bik fi darek 😍 \n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.facebook.com/cs.esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.instagram.com/ieee.cs.esprit/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.linkedin.com/company/cs-esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttp://computer-esprit.ieee.tn/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nTansanèch fi follow/like ken mech 3amel 👀🤣\nFINALLY, 3andek fekra mte3 event wela project? T7eb t7assenni ?\nMy Master {tagFozz} yestana fik 🤩🤩")
+    await usr.send(f"Ahlaa ahla {tag}, mara7bee bik fi darek 😍 \n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.facebook.com/cs.esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.instagram.com/cs.esprit/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttps://www.linkedin.com/company/cs-esprit\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nhttp://computer-esprit.ieee.tn/\n●▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬▬▬●\nTansanèch fi follow/like ken mech 3amel 👀🤣\nFINALLY, 3andek fekra mte3 event wela project? T7eb t7assenni ?\nMy Master {tagFozz} yestana fik 🤩🤩")
 '''
   #log channel
-  log = client.get_channel(867890807759831060)
+  log = client.get_channel(869462857492856883)
   if not message.guild:
     await log.send(f"{tag}: {message.content}")
 
@@ -176,16 +186,19 @@ async def display(ctx):
 @client.command(pass_context=True)
 async def help(ctx):
   embed = discord.Embed(
+    title= "Bot commands",
+    description="Hedhoum el available commands 7aliyan^^",
     colour = discord.Colour.orange()
   )
-  embed.set_author(name="Help")
-  embed.add_field(name="!clear",value="nfassa5 el chat",inline=False)
-  embed.add_field(name="!meme",value="nsarbik meme men subreddit, tnajem ta5tar esm el subreddit, default subreddit heya ProgrammerHumor",inline=False)
-  embed.add_field(name="!play",value="n5adem ghna, het esm el gho,aya | link youtube w taw nsarbik",inline=False)
+  embed.set_image(url = "https://cdn.discordapp.com/attachments/867880055182589975/870346399525535754/img.png")
+  #embed.set_author(name="Hedhoum el available commands 7aliyan^^")
+  embed.add_field(name="!meme",value="nsarbik meme men subreddit, tnajem ta5tar esm el subreddit, default subreddit heya ProgrammerHumor\n Usage: !meme || !meme esmSubreddit",inline=False)
+  embed.add_field(name="!play",value="n5adem ghna, het esm el gho,aya | link youtube w taw nsarbik\n Usage: !play esmGhonaya || !play linkGhonayaFromYoutube ",inline=False)
   embed.add_field(name="!pause",value="npausi laghneya",inline=False)
   embed.add_field(name="!resume",value="nraja3lek laghneya",inline=False)
   embed.add_field(name="!stop",value="n7abes laghneya w n5alik wa7dek",inline=False)
   embed.add_field(name="!leave",value="n5alik wa7dek",inline=False)
+  embed.add_field(name="!clear",value="nfassa5 el chat, mech ay wa7ed enajem yesta3mel el command hedhi :v",inline=False)
   await ctx.send(embed=embed)
   
 '''
@@ -207,7 +220,7 @@ async def leave(ctx):
     voice_channel = ctx.author.guild.voice_client
     await voice_channel.disconnect()
   else : 
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+    await placement_error(ctx)
 
 @client.command(brief="Plays a single video, from a youtube URL")
 async def play(ctx,*url):
@@ -261,7 +274,7 @@ async def play(ctx,*url):
     else:
       await voice.disconnect()
   else:
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+    await placement_error(ctx)
 
 
 #download song
@@ -296,7 +309,7 @@ async def pause(ctx):
     id = ctx.message.guild.id
     players[id].pause()
   else:
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+    await placement_error(ctx)
 
 @client.command()
 async def resume(ctx):
@@ -304,7 +317,7 @@ async def resume(ctx):
     id = ctx.message.guild.id
     players[id].resume()
   else:
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+    await placement_error(ctx)
 
 @client.command()
 async def stop(ctx):
@@ -312,7 +325,7 @@ async def stop(ctx):
     id = ctx.message.guild.id
     players[id].stop()
   else:
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi 5oddim-cmd🤬")
+    await placement_error(ctx)
 
 
 '''@client.command()
@@ -383,8 +396,33 @@ async def meme(ctx,subredditName="ProgrammerHumor"):
     embed.set_image (url = url)
     await ctx.send(embed = embed)
   else : 
-    await ctx.send("Sè7bi barra ekteb el command hedhi fi memes | 5oddim-cmd🤬")
+    await placement_error_meme(ctx)
 
+
+@client.command()
+async def jaweb(ctx,id,*args):
+
+  member = await client.fetch_user(id)
+
+  output=""
+  for word in args:
+    output+=word + " "
+  await member.send(output)
+
+@client.command()
+async def fetchDrive(ctx):
+  tagRoleDev = "<@&"+str(871744335849340959)+">"
+  FOLDER_ID = '1K865vIqaeVnbKmcNnnGEmFcf0ufUN09b'
+  filesList = await fetchData(FOLDER_ID)
+  embed = discord.Embed(
+    title= "Meetings Recordings",
+    description="These are the recordings in the drive folder !",
+    colour= discord.Colour.blue()
+  )
+  for file in filesList:
+    embed.add_field(name=file['name'],value = "https://drive.google.com/file/d/"+file['id'],inline=False)
+
+  await ctx.send(tagRoleDev,embed=embed)
 
 
 
@@ -397,7 +435,10 @@ async def clear_error(ctx, error):
     message = command.split("!clear ",1)[1]
     await ctx.send(f"billehi kifeh tconverti -{message}- l int 🤨?")
   
-
+@meme.error
+async def meme_error(ctx, error):
+  if isinstance(error, commands.errors.CommandInvokeError):
+    await ctx.send("Mnin jebt'ha el subreddit hedhi? xD")
 
 @play.error
 async def play_error(ctx, error):
